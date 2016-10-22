d78511ff88ca9716c28948659c3789a4445e584c
c-harris <c.harris@communicationinnovations.com.au>
2016-10-22 17:24:01 +1000

    Update to replace a tab with 4 spaces
    the update will help to to maintain psr2  compliance in generated model

--- src/Commands/MakeModelsCommand.php
+++ src/Commands/MakeModelsCommand.php
@@ -207,7 +207,7 @@ class MakeModelsCommand extends GeneratorCommand
 
         $class = str_replace('{{table}}', 'protected $table = \'' . $table . '\';', $class);
         
-        $class = str_replace('{{primaryKey}}', $properties['primaryKey'] ? ('protected $primaryKey = \'' . $properties['primaryKey'] . '\';' . "\r\n\r\n\t") : '', $class);
+        $class = str_replace('{{primaryKey}}', $properties['primaryKey'] ? ('protected $primaryKey = \'' . $properties['primaryKey'] . '\';' . "\r\n\r\n\    ") : '', $class);
         
         $class = str_replace('{{extends}}', $extends, $class);
         $class = str_replace('{{shortNameExtends}}', explode('\\', $extends)[count(explode('\\', $extends))-1], $class);
